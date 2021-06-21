48db7a0e113684fe8f3626fda00a2d7b042cb9b7
Bryan <bryan.how@live.com>
2021-06-22 06:39:12 +0800

    completed navBarLoader for website

---
+++ src/navbar.js
@@ -0,0 +1,34 @@
+function createNavBar() {
+    const navBar = document.createElement("nav");
+    navBar.classList.add("navBar");
+
+    const home = document.createElement("a");
+    const menu = document.createElement("a");
+    const aboutUs = document.createElement("a");
+    const contactUs = document.createElement("a");
+
+    home.innerHTML = "Home";
+    menu.innerHTML = "Menu";
+    aboutUs.innerHTML = "About Us";
+    contactUs.innerHTML = "Contact Us";
+
+    // to update once the sections' id are defined
+    home.href = "#home";
+    menu.href = "#menu";
+    aboutUs.href = "#about-us";
+    contactUs.href = "#contatc-us";
+
+    navBar.appendChild(home);
+    navBar.appendChild(menu);
+    navBar.appendChild(aboutUs);
+    navBar.appendChild(contactUs);
+
+    return navBar;
+};
+
+function navBarLoader() {
+    const content = document.getElementById("content");
+    content.appendChild(createNavBar());
+};
+
+export default navBarLoader;
