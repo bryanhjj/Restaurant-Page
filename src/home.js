d5da9fa7e9555dc4a2894cdc3b8f0f1a1f9c2485
Bryan <bryan.how@live.com>
2021-06-22 06:38:47 +0800

    completed homeLoader for webpage

---
+++ src/home.js
@@ -0,0 +1,24 @@
+function addP(text) {
+    const p = document.createElement("p");
+    p.textContent = text;
+    return p;
+};
+
+
+function createHome() {
+    const home = document.createElement("div");
+    home.classList.add("home");
+    home.id = "home"; //for linking with the navBar
+
+    home.appendChild(addP("The best restaurant in Asgard!"));
+    home.appendChild(addP("Founded by our lord Odin himself!"));
+
+    return home;
+};
+
+function homeLoader() {
+    const content = document.getElementById("content");
+    content.appendChild(createHome());
+};
+
+export default homeLoader;
