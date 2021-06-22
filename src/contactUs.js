a235a74cc644f8593da048df19740f926c2396be
Bryan <bryan.how@live.com>
2021-06-23 05:25:44 +0800

    completed contactUsLoader

---
+++ src/contactUs.js
@@ -0,0 +1,28 @@
+function addP(text) {
+    const p = document.createElement("p");
+    p.textContent = text;
+    return p;
+};
+
+function contactUs() {
+    const contact = document.createElement("div");
+    contact.classList.add("contact-us");
+    contact.id = "contact-us"; //for linking with the navBar
+
+    const location = document.createElement("img");
+    location.src = "../assets/asgard.jpg";
+    location.alt = "A map showing where our restaurant is located."
+
+    contact.appendChild(addP("Phone: 012 345 678"));
+    contact.appendChild(addP("Email: odinrestaurant@asgard.com"));
+    contact.appendChild(location);
+
+    return contact;
+};
+
+function contactUsLoader() {
+    const content = document.getElementById("footer");
+    content.appendChild(contactUs());
+};
+
+export default contactUsLoader;
